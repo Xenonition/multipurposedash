f53ae291209cf95d2adb26e87cdabb1f66f5db7f
William Nathan <38376527+Xenonition@users.noreply.github.com>
2024-01-23 00:43:17 -0800

    Added progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ st.set_page_config(page_title="Persib Dashboard", layout="wide")
 st.header("Dashboards")
 st.subheader("PO Jersey Successful Transactions")
 col1, col2 = st.columns([3, 2])
-po_conn = st.connection("postgresql", type="sql")
-po_df = po_conn.query('select payment_status, count(payment_status) from "order" GROUP BY payment_status ORDER BY count(payment_status);', ttl="10m")
+with st.spinner('Connecting to Database...'):
+    po_conn = st.connection("postgresql", type="sql")
+    po_df = po_conn.query('select payment_status, count(payment_status) from "order" GROUP BY payment_status ORDER BY count(payment_status);', ttl="10m")
 po_fig = px.pie(po_df,
              values='count',
              names='payment_status'
@@ -20,8 +21,9 @@ col2.dataframe(po_df)
 
 st.subheader("Email Registration")
 col3, col4 = st.columns([3, 2])
-email_conn = st.connection("sql")
-email_df = email_conn.query("Select created_at from notify_me;")
+with st.spinner('Connecting to Database...'):
+    email_conn = st.connection("sql")
+    email_df = email_conn.query("Select created_at from notify_me;")
 email_df['date'] = pd.to_datetime(email_df['created_at'])
 email_count = (
     email_df.groupby(email_df['date'].dt.date)[['date']]
