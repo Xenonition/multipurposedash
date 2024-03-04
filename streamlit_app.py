5200a4f8ccf68c47670d33534f4b946771259a20
William Nathan <38376527+Xenonition@users.noreply.github.com>
2024-03-04 13:53:00 +0700

    hide other dashboards for now

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,32 +7,6 @@ import plotly.express as px
 st.set_page_config(page_title="Persib Dashboard", layout="wide")
 st.header("Dashboards")
 
-col1, col2, col3 = st.columns([2, 2, 1])
-with st.spinner('Connecting to Database...'):
-    po_conn = st.connection("postgresql", type="sql")
-    po_df = po_conn.query('select payment_status, count(payment_status) from "order" GROUP BY payment_status ORDER BY count(payment_status);', ttl=0)
-    
-po_fig = px.pie(po_df,
-             values='count',
-             names='payment_status'
-            )
-col1.subheader("PO Jersey Successful Transactions")
-col1.plotly_chart(po_fig, theme="streamlit", use_container_width=True)
-
-col2.subheader("Ticket Payment Methods")
-days = col3.number_input('Look for payment in for the past X days', step=1)
-payment_conn = st.connection("payments", type="sql")
-payment_df = payment_conn.query('select payment_method, count(payment_method) from "payments" WHERE created_at > CURRENT_DATE - {} AND status = \'SETTLEMENT\' GROUP BY payment_method ORDER BY count(payment_method);'.format(days), ttl=0)
-
-payment_fig = px.pie(payment_df,
-             values='count',
-             names='payment_method'
-            )
-
-col2.plotly_chart(payment_fig, theme="streamlit", use_container_width=True)
-col3.dataframe(po_df)
-col3.dataframe(payment_df)
-
 OLD_DB_WAITLIST_COUNT = 5936
 st.subheader("Email Registration")
 col3, col4 = st.columns([3, 2])
@@ -53,3 +27,30 @@ email_fig = px.line(email_count, x='date', y="count")
 col3.plotly_chart(email_fig, theme="streamlit", use_container_width=True)
 col4.metric("Registered Emails", total, delta)
 col4.dataframe(email_count)
+
+
+# col1, col2, col3 = st.columns([2, 2, 1])
+# with st.spinner('Connecting to Database...'):
+#     po_conn = st.connection("postgresql", type="sql")
+#     po_df = po_conn.query('select payment_status, count(payment_status) from "order" GROUP BY payment_status ORDER BY count(payment_status);', ttl=0)
+    
+# po_fig = px.pie(po_df,
+#              values='count',
+#              names='payment_status'
+#             )
+# col1.subheader("PO Jersey Successful Transactions")
+# col1.plotly_chart(po_fig, theme="streamlit", use_container_width=True)
+
+# col2.subheader("Ticket Payment Methods")
+# days = col3.number_input('Look for payment in for the past X days', step=1)
+# payment_conn = st.connection("payments", type="sql")
+# payment_df = payment_conn.query('select payment_method, count(payment_method) from "payments" WHERE created_at > CURRENT_DATE - {} AND status = \'SETTLEMENT\' GROUP BY payment_method ORDER BY count(payment_method);'.format(days), ttl=0)
+
+# payment_fig = px.pie(payment_df,
+#              values='count',
+#              names='payment_method'
+#             )
+
+# col2.plotly_chart(payment_fig, theme="streamlit", use_container_width=True)
+# col3.dataframe(po_df)
+# col3.dataframe(payment_df)
